bc0fa5f368a8dd255f531567af081297bb1efbd0
MacroEcoMatt <matthewwatson292@gmail.com>
2023-09-18 16:34:07 -0400

    GitHub Packages

--- R/get_env_vars.R
+++ R/get_env_vars.R
@@ -48,20 +48,8 @@
 #' @export
 get_env_vars <- function(point_data, env_vars = "both", date_format = "monthly"){
   #CHECK PACKAGES
-  if(length(find.package(cliamteR, quiet = T)) == 0){
-    devtools::install_github("mikejohnson51/climateR")
-    require(climateR)
-  } else {
-    require(climateR)
-  }
-
-  if(length(find.package(AOI, quiet = T)) == 0){
-    devtools::install_github("mikejohnson51/AOI")
-    require(AOI)
-  } else {
-    require(AOI)
-  }
-
+  try(if(isFALSE(require(climateR)))stop("No package: climateR"))
+  try(if(isFALSE(require(AOI)))stop("No package: AOI"))
   try(if(isFALSE(require(sf)))stop("No package: sf"))
   try(if(isFALSE(require(raster)))stop("No package: raster"))
   try(if(isFALSE(require(terra)))stop("No package: terra"))
